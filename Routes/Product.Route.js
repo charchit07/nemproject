09f02f287a49e19b8b274ac4ed5510d48489857f
Arpit Kumar Jain <arpitjain2444@gmail.com>
2023-02-25 23:30:17 +0530

    Update Product.Route.js

--- Routes/Product.Route.js
+++ Routes/Product.Route.js
@@ -23,7 +23,7 @@ productRoute.get("/", async (req, res) => {
   const rating = req.query.rating
 // const arr=[price,rating]
 // for(let i=0;i<arr.length;i++){
-   if(price){
+ 
    if(category && gender && brand){
     try {
         let productData =await ProductModel.find({$and:[{category: { $regex: `${category}`, $options: "i" }},{gender:gender},{brand: { $regex: `${brand}`, $options: "i" },}]}).sort({price:price})
@@ -90,10 +90,10 @@ productRoute.get("/", async (req, res) => {
     else {
     const product = await ProductModel.find().sort({price:price})
     res.send(product);
-}}
+}
   
   
-     if(rating){
+ 
    if(category && gender && brand){
     try {
         let productData =await ProductModel.find({$and:[{category: { $regex: `${category}`, $options: "i" }},{gender:gender},{brand: { $regex: `${brand}`, $options: "i" },}]}).sort({rating:rating})
@@ -160,7 +160,7 @@ productRoute.get("/", async (req, res) => {
     else {
     const product = await ProductModel.find().sort({rating:rating})
     res.send(product);
-}}
+}
   
   
   
